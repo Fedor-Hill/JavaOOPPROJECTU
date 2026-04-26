2f55ba15b69b912b665a5768f2e958aecd392521
fedya <fedor.hill69@gmail.com>
2026-04-26 16:33:08 +0500

    update right list

--- src/Enums/ACCESS_RIGHT.java
+++ src/Enums/ACCESS_RIGHT.java
@@ -1,11 +1,18 @@
 package Enums;
 
 /**
- * 
+ * @author Meiramkhan Alinur
+ * @author Kim Alina
  */
 public enum ACCESS_RIGHT {
     ALL,
     CREATE_STUDENT,
     EDIT_STUDENT,
-    DELETE_STUDENT
+    DELETE_STUDENT,
+    VIEW_STUDENT,
+    CREATE_REQUEST,
+    VIEW_REQUEST,
+    RESPONSE_REQUEST,
+    CREATE_NOTIFICATION,
+    DELETE_NOTIFICATION
 }
